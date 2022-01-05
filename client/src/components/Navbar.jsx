fe73db1a129f0623369cbbfb8c64d9d0df904adc
Jerry Chein <breakingkyo@gmail.com>
2022-01-06 00:50:58 +0800

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import { GiCyberEye } from 'react-icons/gi';
+
+const NavbarItem = ({ title, classProps }) => {
+  return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
+};
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+      <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+        <GiCyberEye className='text-white text-5xl w-32 cursor-pointer' />
+      </div>
+      <ul className='md:flex hidden list-none flex-row justify-between items-center flex-initial text-[#fff]'>
+        {['Market', 'Exchange', 'Tutorials', 'Wallets'].map((item, index) => (
+          <NavbarItem key={item + index} title={item} />
+        ))}
+        <li className='bg-[#2953e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+          Login
+        </li>
+      </ul>
+      <div className='flex relative'>
+        {toggleMenu ? (
+          <AiOutlineClose
+            fontSize={28}
+            className='md:hidden text-white cursor-pointer'
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <HiMenu
+            fontSize={28}
+            className='md:hidden text-white cursor-pointer'
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <ul className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'>
+            <li className='text-xl w-full my-2'>
+              <AiOutlineClose onClick={() => setToggleMenu(false)} />
+            </li>
+            {['Market', 'Exchange', 'Tutorials', 'Wallets'].map(
+              (item, index) => (
+                <NavbarItem
+                  key={item + index}
+                  title={item}
+                  classProps='my-2 text-lg'
+                />
+              )
+            )}
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
